f96f868156cbf58014b44b540c2a4dc12213d545
Flying_Fish <johann.lacroix@kapsloc.com>
2025-01-29 23:58:32 +0100

    backup before structural change

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import styles from "./Gallery.module.scss";
 import Banner from "../Banner/Banner";
 import Card from "../Card/Card";
 
 const Gallery = () => {
+  const [annonces, setAnnonces] = useState([]);
+
+  useEffect(() => {
+    fetch("/annonces.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur lors du chargement des données");
+        }
+        return response.json();
+      })
+      .then((data) => setAnnonces(data))
+      .catch((error) =>
+        console.error("Erreur de chargement des données :", error)
+      );
+  }, []);
+
   return (
     <div className={styles.container}>
       <Banner />
+
+      {/* Grid contenant toutes les Cards */}
       <div className={styles.gallery}>
-        <Card />
+        {annonces.map((annonce) => (
+          <Card key={annonce.id} annonce={annonce} />
+        ))}
       </div>
     </div>
   );
